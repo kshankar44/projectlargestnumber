c287b10e1086483be23f2fb7b207b98af124f537
K Shankar <01fe22bca043@kletech.ac.in>
2024-02-03 11:58:11 +0530

    code changes related to prime number

---
+++ primenumber.java
@@ -0,0 +1,22 @@
+class primenumber{
+
+	public static void main(String[] args) {
+		int min = 2;
+		int max = 100;
+		for(int n=min;n<=max;n++) {
+			if(isPrime(n)) {
+				System.out.println(n);
+			}
+		}
+	}
+	public static boolean isPrime(int num) {	
+		for(int i = 2; i <= num/i; ++i) {
+			if(num % i == 0) {
+				return false;
+			}
+		}
+		return true;
+	}
+}
+
+
